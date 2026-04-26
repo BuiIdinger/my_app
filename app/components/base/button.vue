6286eecd3d1d6a57f8aba2b6784f71198196e6e2
Buildinger <shaydenseymour@gmail.com>
2026-04-27 10:38:35 +1200

    Initial commit

---
+++ app/components/base/button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-[10px]">
+    <button class="
+    bg-[#0693e3] border-b-[4px] border-[#8ed1fc]/50 font-bold uppercase text-[15px]
+      rounded-[12px] px-[20px] py-[14px] text-white active:scale-[.98] active:opacity-95
+      active:text-opacity-35 duration-300 w-full select-none"
+    >
+      qweqwe
+      {{ props.text }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string,
+  omit_background: boolean,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  omit_background: false,
+});
+</script>
---
+++ app/pages/onboarding/name/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-dvw h-dvh relative">
+
+    <div
+    class="flex flex-col gap-y-[32px] justify-center items-center
+      w-full h-full absolute"
+    >
+      <div>
+        <h1>Your name</h1>
+      </div>
+      <div>
+        <nuxt-img src="/bird.png" class="w-[140px] h-[140px] object-contain" />
+      </div>
+
+      <div class="font-semibold opacity-50">
+        <p>Always free, only on duolingo.</p>
+      </div>
+
+    </div>
+
+    <div class="absolute bottom-[25px] left-0 right-0">
+      <base_button
+      @click.prevent="on_click"
+      text="Continue with email"
+      />
+      <base_button
+      text="I already have an account"
+      omit_background
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import base_button from "~/components/base/button.vue";
+import brandmark_variant_two from "~/components/brandmarks/variant_two.vue";
+
+const on_click = (): void => {
+  console.warn("clicked");
+}
+</script>
